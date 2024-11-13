126353014946d9eb0a1c3ae6e246fa0d4a7bb2d4
Ganesh Gautam <85697985+ganeshgautam-official@users.noreply.github.com>
2024-11-13 15:04:46 +0545

    Update debug.sql

--- Queries/debug.sql
+++ Queries/debug.sql
@@ -1,21 +1,24 @@
+-- Raw table
 SELECT
     *
 FROM
     sales_data_raw
 LIMIT 10;
 
-
+-- cleaned table
 SELECT
     *
 FROM
     sales_data
 LIMIT 10;
 
+-- Row count
 SELECT
     COUNT(*)
 FROM
     sales_data;
 
+-- Distinct customer count
 SELECT
     DISTINCT CustomerId 
 FROM
